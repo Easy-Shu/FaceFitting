1800fe8ceaa0238b04bccfbfa01457e398202586
Dhiego Magalhães <dhiegomaga@gmail.com>
2017-05-16 00:39:25 -0300

    Added first files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Face Fitting
+
+Little project in python based on project from Patrick Hubber (https://github.com/patrikhuber/eos). Idea is to fit a morphable model (the basel face model) to face image, given keypoints in the image and in the face model. The project is still in its very beginning. 
+
+## Requirements
+
+* Python 3.5, with some standard libraries (numpy, opencv 3...)
+* Basel Face Model (possibly the .mat file)
+* Image with keypoint file. I will provide one. 
---
+++ keypoint_drawing.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+keypoint_file = 'image_0010.pts'
+image_file = 'image_0010.png'
+points = 68
+data = []
+
+# Read points:
+with open(keypoint_file) as file:
+	line = file.readline()
+	print(line)
+	line = file.readline()
+	print(line)
+	line = file.readline()
+	print(line)
+	
+	for x in range(points):
+		line = file.readline()
+		data.append([float(x) for x in line.split()])
+		print(line)
+		
+	line = file.readline()
+	print(line)
+
+file.close()
+
+# Load and display image_0010
+img = cv2.imread(image_file)
+
+# Draw keypoints
+font = cv2.FONT_HERSHEY_SIMPLEX
+for idx, point in enumerate(data):
+	x, y = point
+	coord = (int(x), int(y))
+	cv2.circle(img, (int(x),int(y)), 1,(0,0,255), 1)
+	cv2.putText(img, str(idx), coord, font, 0.3, (0,0,255))
+	cv2.imshow('imagem...', img)
+	cv2.waitKey(0)
+
+
+cv2.destroyAllWindows()
+
